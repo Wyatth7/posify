4bed08e70a4439c9a789c63393d24b3c9cc70303
wyatth7 <whardin1010@gmail.com>
2022-01-18 10:07:20 -0500

    Updated potential server vulnerability

---
+++ server/src/models/UserModel.ts
@@ -0,0 +1,7 @@
+// There will be many users with different roles within the confines
+// of this project. There will be store owners, managers, employees, etc.
+// Due to this, there will need to be a different model in which a
+// specific resturant is created, and users are added.
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({});
